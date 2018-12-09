78bb90f9fb3995a3f8cbc78a77bbd24f8b4ba142
hasnain003 <mdhasnain839@gmail.com>
2018-12-10 01:06:16 +0530

    Add SquareAPI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './component/Map';
+import SquareAPI from "./API/"
+
 
 class App extends Component {
+  componentDidMount() {
+    SquareAPI.search({
+      near:"Austin,TX",
+      query:"facos",
+      limit:10
+    }).then(results => console.log(results));
+  }
+
   render() {
     return (
       <div className="App">
