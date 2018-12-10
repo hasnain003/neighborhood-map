9b133f1b5573e17bc2217da9b3ef597c6f4f7a22
hasnain003 <mdhasnain839@gmail.com>
2018-12-10 11:23:53 +0530

    Add markers and it infowindow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Map from './component/Map';
-import SquareAPI from "./API/"
+import SquareAPI from "./API/index"
 
 
 class App extends Component {
@@ -15,18 +14,36 @@ class App extends Component {
       zoom:12
     };
   }
+  handleMarkerClick = (marker) => {
+    marker.isOpen=true;
+    this.setState({markers: Object.assign(this.state.markers,marker)});
+  };
   componentDidMount() {
     SquareAPI.search({
-      near:"Austin,TX",
-      query:"facos",
-      limit:10
-    }).then(results => console.log(results));
+      query: 'coffee',
+      near: 'Eau Claire, WI',
+      limit: 10
+    }).then(results => {
+      const { venues } = results.response;
+      const { center } = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isVisible: true,
+          id: venue.id
+        };
+      });
+      this.setState({ venues, markers, center });
+      console.log(results);
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map {...this.state} handleMarkerClick={this.handleMarkerClick}/>
       </div>
     );
   }
