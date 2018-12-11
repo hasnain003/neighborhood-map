02adbf113f6fcc83c0749a324eb6a1fb5a85c6a2
hasnain003 <mdhasnain839@gmail.com>
2018-12-11 21:43:26 +0530

    Add Close Marker method

--- src/App.js
+++ src/App.js
@@ -14,9 +14,25 @@ class App extends Component {
       zoom:12
     };
   }
-  handleMarkerClick = (marker) => {
+
+  closeAllMarkers = () => {
+    const markers = this.state.markers.map(marker =>{
+      marker.isOpen = false;
+      return marker;
+    });
+    this.setState({markers: Object.assign(this.state.markers,markers) });
+  };
+
+  handleMarkerClick = marker => {
+    this.closeAllMarkers();
     marker.isOpen=true;
     this.setState({markers: Object.assign(this.state.markers,marker)});
+
+    const venue = this.state.venues.find(venue => venue.id === marker.id);
+    SquareAPI.getVenueDetails(marker.id).then(res => {
+      const newVenue = Object.assign(venue, res.response.venue);
+      this.setState({ venues: Object.assign(this.state.venues, newVenue) });
+    });   
   };
   componentDidMount() {
     SquareAPI.search({
