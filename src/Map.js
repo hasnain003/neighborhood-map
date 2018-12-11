f62088f89147f9ab92de0daaebb0e8fc2a87701a
hasnain003 <mdhasnain839@gmail.com>
2018-12-11 23:27:54 +0530

    Add image in info window

--- src/Map.js
+++ src/Map.js
@@ -1,48 +1,79 @@
+/* global google */
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker , InfoWindow } from "react-google-maps"
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from 'react-google-maps';
 
 const MyMapComponent = withScriptjs(
-	withGoogleMap((props) =>(
-  		<GoogleMap
-    	defaultZoom={8}
-    	zoom={props.zoom}
-    	defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    	center={{
-    		lat: parseFloat(props.center.lat),
-    		lng: parseFloat(props.center.lng)
-    	}}
-  	>
-    {props.markers 	&&
-     props.markers
-     .filter( marker => marker.isVisible)
-     .map((marker,idx) => (
-     	<Marker 
-     		key={idx} 
-     		position={{ lat: marker.lat, lng: marker.lng }}
-     		onClick={() => props.handleMarkerClick(marker)}
-     	>
-     	{marker.isOpen && (
-     		<InfoWindow>
-     			<p>Hello</p>
-     		</InfoWindow>
-     	)}
-     	</Marker>
-     	))}
- 	 </GoogleMap>
-))
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={8}
+      zoom={props.zoom}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      center={{
+        lat: parseFloat(props.center.lat),
+        lng: parseFloat(props.center.lng)
+      }}
+    >
+      {props.markers &&
+        props.markers
+          .filter(marker => marker.isVisible)
+          .map((marker, index, arr) => {
+            const venueInfo = props.venues.find(
+              venue => venue.id === marker.id
+            );
+            return (
+              <Marker
+                key={index}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                onClick={() => props.handleMarkerClick(marker)}
+                animation={
+                  arr.length === 1
+                    ? google.maps.Animation.BOUNCE
+                    : google.maps.Animation.DROP
+                }
+              >
+                {marker.isOpen &&
+                  venueInfo.bestPhoto && (
+                    <InfoWindow>
+                      <React.Fragment>
+                        <p className="font-weight-bold">{venueInfo.name}</p>
+                        <hr />
+                        <p>
+                          {venueInfo.location.formattedAddress[0]}
+                          <br />
+                          {venueInfo.location.formattedAddress[1]}
+                        </p>
+                        <img
+                          src={`${venueInfo.bestPhoto.prefix}200x200${
+                            venueInfo.bestPhoto.suffix
+                          }`}
+                          alt={`${venueInfo.name}`}
+                        />
+                      </React.Fragment>
+                    </InfoWindow>
+                  )}
+              </Marker>
+            );
+          })}
+    </GoogleMap>
+  ))
 );
 
 export default class Map extends Component {
-
-	render() {
-		return(
-			<MyMapComponent
-  				{...this.props}
-  				googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAr7_bhm_T4gTYGPYYsdYzdkmnak283zpI"
-  				loadingElement={<div style={{ height: `100%` }} />}
-  				containerElement={<div style={{ height: `400px` }} />}
-  				mapElement={<div style={{ height: `100%` }} />}
-			/>
-		);
-	}
+  render() {
+    return (
+      <MyMapComponent
+        {...this.props}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAr7_bhm_T4gTYGPYYsdYzdkmnak283zpI"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div id="map" className="col-12 col-sm-9 d-block" />}
+        mapElement={<div className="d-flex" style={{ height: `100%` }} />}
+      />
+    );
+  }
 }
