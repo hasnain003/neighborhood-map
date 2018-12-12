26ae0833e1e29b8e3a8ad2179393204a5f6757ec
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 14:16:30 +0530

    Add ListItem and paas props to it

--- src/component/VenueList.js
+++ src/component/VenueList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ListItem from './listItem'
 
 class VenueList extends Component {
 	render() {
 		return(
 			<ol className="venueList">
-
+				{this.props.venues &&
+					this.props.venues.map((venue,idx) =>  (<ListItem key={idx} {...venue} handleListItemClick={this.props.handleListItemClick}/>
+				))}
 			</ol>
 		);
 	}
