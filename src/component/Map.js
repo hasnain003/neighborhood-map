8358fcd98b91e82bfeba27d2cb1dc56c2239433d
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 10:12:52 +0530

    Error fixed

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, 
+	withGoogleMap, 
+	GoogleMap, 
+	Marker,
+	InfoWindow 
+	} from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
+const MyMapComponent = withScriptjs(
+	withGoogleMap((props) => (
+  		<GoogleMap
+    	defaultZoom={8}
+    	zoom={props.zoom}
+    	defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    	center={{
+    		lat: parseFloat(props.center.lat),
+    		lng: parseFloat(props.center.lng)
+    	}}
+  	>
+  	console.log("Outside");
+    {props.markers 	&&
+     props.markers
+     .filter( marker => marker.isVisible)
+     .map((marker, idx) => {
+            const venueInfo = props.venues.find(
+              venue => venue.id === marker.id
+            );
+            return(
+              <Marker 
+                key={idx} 
+                position={{ lat: marker.lat, lng: marker.lng }}
+                onClick={() => props.handleMarkerClick(marker)}
+              >
+              {marker.isOpen &&
+                venueInfo.bestPhoto &&  (
+                <InfoWindow>
+                  <React.Fragment>
+                    <img
+                      src={`${venueInfo.bestPhoto.prefix}200x200${
+                          venueInfo.bestPhoto.suffix
+                        }`}
+                      alt={`${venueInfo.name}`}
+                    />
+                    <p>{venueInfo.name}</p>
+                  </React.Fragment>
+                </InfoWindow>
+             )}
+              console.log("hi,I am in marker");
+             </Marker>
+          );
+     	})}
+ 	 </GoogleMap>
 ))
-
+);
 
 export default class Map extends Component {
 
 	render() {
 		return(
 			<MyMapComponent
-  				isMarkerShown
+  				{...this.props}
   				googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAr7_bhm_T4gTYGPYYsdYzdkmnak283zpI"
   				loadingElement={<div style={{ height: `100%` }} />}
   				containerElement={<div style={{ height: `400px` }} />}
