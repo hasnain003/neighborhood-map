adf90f7634e674b31ca98768e04e495aed9aac50
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 11:01:48 +0530

    intialize file

---
+++ src/component/SideBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+	render() {
+		return(
+			<div className="sideBar">
+			</div>
+		);
+	}
+}
+
+export default SideBar;
