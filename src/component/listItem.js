8bed35cde6993055f75da43f99dca6b252aab5af
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 11:05:17 +0530

    intialize file

---
+++ src/component/listItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+	render() {
+		return(
+			<li className="listItem">
+			</li>
+		);
+	}
+}
+
+export default ListItem;
