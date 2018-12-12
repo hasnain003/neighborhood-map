3fd5c5736bbaf26cda9fda0abecd42c572224558
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 11:02:57 +0530

    intialize file

---
+++ src/component/VenueList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VenueList extends Component {
+	render() {
+		return(
+			<ol className="venueList">
+
+			</ol>
+		);
+	}
+}
+
+export default VenueList;
