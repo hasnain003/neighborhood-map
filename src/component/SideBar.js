9087ba939ff661837b6cc17cd7239ccc1a37ff55
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 14:06:45 +0530

    Add input and handleChange method

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import VenueList from './VenueList'
 
 class SideBar extends Component {
+	constructor() {
+		super();
+		this.state= {
+			query:""
+		};
+	}
+
+	handleChange =e => {
+		this.setState({query : e.target.value})
+		const markers = this.props.venues.map(venue => {
+			const isMatched = venue.name
+			.toLowerCase()
+			.includes(e.target.value.toLowerCase());
+			const marker = this.props.markers.find(marker => marker.id === venue.id);
+			if(isMatched) {
+				marker.isVisible = true;
+			} else {
+				marker.isVisible = false;
+			}
+			return marker;
+		});
+		this.props.updateSuperState({markers});
+	}
+
 	render() {
 		return(
 			<div className="sideBar">
+				<input type={"search"} id={"search"} placeholder={"filter venues"} onChange={this.handleChange}/>
+				<VenueList {...this.props} handleListItemClick={this.props.handleListItemClick}/>
 			</div>
 		);
 	}
