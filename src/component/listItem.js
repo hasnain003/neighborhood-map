1bd5f97d5ea574ca899393b6fc58202f26b0a55c
hasnain003 <mdhasnain839@gmail.com>
2018-12-12 14:18:53 +0530

    Add icon image and name of list item

--- src/component/listItem.js
+++ src/component/listItem.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class ListItem extends Component {
 	render() {
 		return(
-			<li className="listItem">
+			<li className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+				<img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
+				{this.props.name}
 			</li>
 		);
 	}
