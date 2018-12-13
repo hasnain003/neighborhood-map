0b249a460b6244f4019052d3aa5281a63559d654
hasnain003 <mdhasnain839@gmail.com>
2018-12-13 23:39:56 +0530

    Add aria-label

--- src/component/VenueList.js
+++ src/component/VenueList.js
@@ -4,7 +4,7 @@ import ListItem from './listItem'
 class VenueList extends Component {
 	render() {
 		return(
-			<ol className="venueList">
+			<ol className="venueList" aria-label="List of Coffe Shop">
 				{this.props.venues &&
 					this.props.venues.map((venue,idx) =>  (<ListItem key={idx} {...venue} handleListItemClick={this.props.handleListItemClick}/>
 				))}
