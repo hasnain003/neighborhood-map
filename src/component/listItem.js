a3e44ef728c9eaff8ce39e09413b6619667df451
hasnain003 <mdhasnain839@gmail.com>
2018-12-13 23:36:52 +0530

    Add tab-index and role

--- src/component/listItem.js
+++ src/component/listItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ListItem extends Component {
 	render() {
 		return(
-			<li className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+			<li className="listItem" tabIndex={0} role="button" onClick={() => this.props.handleListItemClick(this.props)}>
 				<img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
 				{this.props.name}
 			</li>
